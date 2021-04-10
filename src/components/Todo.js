3c010988203815f82604bb7b7a61c7ed994b5c58
halliwell2046 <halliwell2046@yahoo.com>
2021-04-10 13:21:36 -0400

    fixed error

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,7 @@ import React from 'react'
 class Todo extends React.Component {
     render () {
         return (
-            <div className={`todo ${this.props.todo.completed ? "completed" : ""}`}
+            <div className={`todo ${this.props.task.completed ? "completed" : ""}`}
             onClick={() => {this.props.todoToggle(this.props.task.id)}}>
                 <p>{this.props.task.newTask}</p>
 
